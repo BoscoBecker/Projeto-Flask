5846497fe11ca75a8b6511fd099cbf66d575d10b
João bosco <joao_boscomf@hotmail.com>
2018-12-03 11:03:49 -0200

    # implementando travis

--- Projeto-Flask/cria_banco.py
+++
@@ -1,7 +0,0 @@
-
-from tabelas import postagem, db
-
-lista_de_postagem = [ postagem, ]
-
-db.connect()
-db.create_tables(lista_de_postagem)
--- Projeto-Flask/inicia_server.bat
+++
@@ -1,4 +0,0 @@
-set FLASK_APP=app.py
-set FLASK_DEBUG=1
-
-flask run --port 8082 --host 127.0.0.1
